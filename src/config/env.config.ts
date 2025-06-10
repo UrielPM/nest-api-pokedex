84274193f49c68433b83ad1b24b905f8e17ab6b2
Uriel Mora <upm1808@gmail.com>
2025-06-10 13:52:15 -0600

    Feat: Joi ValidationSchema

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,9 +1,6 @@
-
-
 export const EnvConfiguration = () => ({
-    enviroment: process.env.NODE_ENV || 'dev', 
-    mongodb: process.env.MONGODB,
-    port: process.env.PORT || 3002,
-    defaulLimit: +process.env.DEFAULT_LIMIT! || 7,
-
-})
+  enviroment: process.env.NODE_ENV || 'dev',
+  mongodb: process.env.MONGODB,
+  port: process.env.PORT || 3002,
+  defaulLimit: +process.env.DEFAULT_LIMIT! || 7,
+});
